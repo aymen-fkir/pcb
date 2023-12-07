f6d2b48b42727de8c6afdee90942eaa11c4f3292
Aymen <aymenfkir23@gmail.com>
2023-12-07 16:45:27 +0100

    finished the signin and signup page

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ function insert_into_db(table_name, data, callback) {
             return;
         }
         else{
-            console.log(data)
             connection.query('INSERT INTO ?? SET ?', [table_name, data], (insertErr, results) => {
                 close_connection(connection); // Close connection regardless of query result
     
@@ -57,6 +56,27 @@ function insert_into_db(table_name, data, callback) {
     });
 }
 
+function fetch_db(querry,callback){
+    create_connection((err, connection) => {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        else{
+            connection.query(querry, (insertErr, results) => {
+                close_connection(connection); // Close connection regardless of query result
+    
+                if (insertErr) {
+                    callback(insertErr, null);
+                } else {
+                    callback(null, results);
+                }
+            });
+
+        }
+    });
+}
+
 app.get("/", (req, res) => {
     res.sendFile(Login_file);
 });
@@ -65,6 +85,19 @@ app.post("/signup", (req, res) => {
     const form = req.body;
     console.log(form);
     insert_into_db('users', form, (err, ress) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            console.log("DONE");
+            res.send(ress);
+        }
+    });
+});
+
+app.post("/signin", (req, res) => {
+    const form = req.body;
+    const querry = "SELECT * FROM users where email = '"+ form.email_signin +"' and password = '"+form.password_signin+"'"; 
+    fetch_db(querry, (err, ress) => {
         if (err) {
             res.status(500).send(err);
         } else {
